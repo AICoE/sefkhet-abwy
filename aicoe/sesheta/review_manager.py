beb8f5c35d4946b7885bf35fe407039f8dd0ef2a
Christoph Görn <goern@redhat.com>
2019-10-02 11:39:03 +0200

    :sparkles: added WIP handling on PRs
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- aicoe/sesheta/review_manager.py
+++ aicoe/sesheta/review_manager.py
@@ -21,6 +21,7 @@
 
 import asyncio
 import pathlib
+import logging
 
 from datetime import datetime
 
@@ -29,23 +30,37 @@ from octomachinery.app.routing import process_event_actions
 from octomachinery.app.routing.decorators import process_webhook_payload
 from octomachinery.app.runtime.context import RUNTIME_CONTEXT
 
+from thoth.common import init_logging
+
+
+__version__ = "0.1.0"
+
+
+init_logging()
+
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.info(f"AICoE's Review Manager, Version v{__version__}")
+
 
 @process_event_actions("pull_request", {"opened", "edited"})
 @process_webhook_payload
-async def on_pr_check_wip(*, action, number, pull_request, repository, sender, organization, installation):
+async def on_pr_check_wip(*, action, number, pull_request, repository, sender, organization, installation, changes):
     """React to an opened or changed PR event.
 
     Send a status update to GitHub via Checks API.
     """
     github_api = RUNTIME_CONTEXT.app_installation_client
 
-    check_run_name = "Work-in-progress state 🤖"
+    check_run_name = "Sesheta work-in-progress state"
 
     pr_head_branch = pull_request["head"]["ref"]
     pr_head_sha = pull_request["head"]["sha"]
     repo_url = pull_request["head"]["repo"]["url"]
+    issue_url = pull_request["issue_url"]
 
     check_runs_base_uri = f"{repo_url}/check-runs"
+
+    issue_labels_response = await github_api.getitem(f"{issue_url}/labels", preview_api_version="symmetra")
 
     resp = await github_api.post(
         check_runs_base_uri,
@@ -70,6 +85,25 @@ async def on_pr_check_wip(*, action, number, pull_request, repository, sender, o
 
     is_wip_pr = any(m in pr_title for m in wip_markers)
 
+    if is_wip_pr:
+        try:
+            await github_api.post(
+                f"{issue_url}/labels",
+                preview_api_version="symmetra",
+                data={"labels": ["do-not-merge/work-in-progress"]},
+            )
+        except gidgethub.BadRequest as err:
+            if err.status_code != 202:
+                _LOGGER.error(err)
+    else:
+        try:
+            await github_api.delete(
+                f"{issue_url}/labels/do-not-merge%2Fwork-in-progress", preview_api_version="symmetra"
+            )
+        except gidgethub.BadRequest as err:
+            if err.status_code != 200:
+                _LOGGER.error(err)
+
     await github_api.patch(
         check_runs_updates_uri,
         preview_api_version="antiope",
@@ -79,7 +113,7 @@ async def on_pr_check_wip(*, action, number, pull_request, repository, sender, o
             "conclusion": "success" if not is_wip_pr else "neutral",
             "completed_at": f"{datetime.utcnow().isoformat()}Z",
             "output": {
-                "title": "🤖 This PR is not Work-in-progress: Good to go",
+                "title": "🤖 This PR is NOT work-in-progress: Good to go",
                 "text": "Debug info:\n"
                 f"is_wip_pr={is_wip_pr!s}\n"
                 f"pr_title={pr_title!s}\n"
@@ -88,7 +122,7 @@ async def on_pr_check_wip(*, action, number, pull_request, repository, sender, o
             }
             if not is_wip_pr
             else {
-                "title": "🤖 This PR is Work-in-progress: " "It is incomplete",
+                "title": "🤖 This PR is work-in-progress: " "It is incomplete",
                 "text": "Debug info:\n"
                 f"is_wip_pr={is_wip_pr!s}\n"
                 f"pr_title={pr_title!s}\n"
@@ -100,4 +134,7 @@ async def on_pr_check_wip(*, action, number, pull_request, repository, sender, o
 
 
 if __name__ == "__main__":
-    run_app(name="review-manager", version="0.1.0", url="https://github.com/apps/review-manager")
+    _LOGGER.setLevel(logging.DEBUG)
+    _LOGGER.debug("Debug mode turned on")
+
+    run_app(name="review-manager", version=__version__, url="https://github.com/apps/review-manager")
