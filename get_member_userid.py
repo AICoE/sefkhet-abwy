e7c9c9fb5df86c4d4a32e37f6fa94fcd5099190c
Christoph Görn <goern@redhat.com>
2021-09-17 16:32:24 +0200

    :sparkles: tiny tool to get member user id from a channel (#166)
    Signed-off-by: Christoph Görn <goern@redhat.com>

---
+++ get_member_userid.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Sefkhet-Abwy
+# Copyright(C) 2019 Christoph Görn
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Sesheta's actions."""
+
+
+import asyncio
+import os
+import logging
+import typing
+
+from httplib2 import Http
+from apiclient.discovery import build, build_from_document
+from httplib2 import Http
+from oauth2client.service_account import ServiceAccountCredentials
+
+from thoth.common import init_logging
+
+
+init_logging()
+
+_LOGGER = logging.getLogger(__name__)
+
+THOTH_DEVOPS_SPACE = os.getenv("SESHETA_THOTH_DEVOPS_SPACE", None)
+AIOPS_DEVOPS_SPACE = os.getenv("SESHETA_AIOPS_DEVOPS_SPACE", None)
+
+
+scopes = ["https://www.googleapis.com/auth/chat.bot"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name("sesheta-chatbot-b5a97b40eeab.json", scopes)
+http_auth = credentials.authorize(Http())
+
+chat = build("chat", "v1", http=http_auth)
+
+response = chat.spaces().members().list(parent=THOTH_DEVOPS_SPACE)
+
+if response is not None:
+    r = response.execute()
+
+for member in r["memberships"]:
+    print(f"\"{member['member']['displayName']}\": \"{member['member']['name']}\",")
